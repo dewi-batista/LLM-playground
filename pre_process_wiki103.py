fcc32def5fcefa082c0d53fb4c0cfe29f9d623fb
dewibatista <dewibatista@gmail.com>
2025-12-31 17:58:48 +0100

    update

--- pre_process_wiki103.py
+++ pre_process_wiki103.py
@@ -4,7 +4,16 @@ from tqdm import tqdm
 
 data = load_dataset("wikitext", "wikitext-103-v1")
 lines = data["train"]["text"]
+
+replacements = {
+    " @-@ ": "-",
+    " @.@ ": ".",
+    " @,@ ": ",",
+}
+
 with open("./data/wiki103.txt", "w", encoding="utf-8") as f:
-    for line in tqdm(lines, desc="Writing wiki103"):
+    for line in tqdm(lines, desc="Writing wiki103.txt"):
         if line.strip():
-            f.write(line.replace(" @-@ ", "-") + " ")
+            for k, v in replacements.items():
+                line = line.replace(k, v)
+            f.write(line + " ")
