c6281b7a47d24fc5d15c95584d8403ffb56a233b
dewibatista <dewibatista@gmail.com>
2025-12-31 15:35:41 +0100

    update

--- pre_process_wiki103.py
+++ pre_process_wiki103.py
@@ -5,4 +5,4 @@ data = load_dataset("wikitext", "wikitext-103-v1")
 with open("./data/wiki103.txt", "w", encoding="utf-8") as f:
     for line in data["train"]["text"]:
         if line.strip():
-            f.write(line + " ")
+            f.write(line.replace(" @-@ ", "-") + " ")
