f69c9d66d5e2e4c3994b479402df9d3976f37145
dewibatista <dewibatista@gmail.com>
2026-01-01 16:03:12 +0100

    update

--- data/pre_processing_scripts/pre_process_openwebtext.py
+++ data/pre_processing_scripts/pre_process_openwebtext.py
@@ -20,8 +20,8 @@ data = load_dataset("Skylion007/openwebtext", streaming=True)
 
 bytes_written = 0
 MAX_BYTES = 10 * 1024**3 # (10 GiB)
-with open("./data/bookcorp.txt", "w", encoding="utf-8") as f:
-    for line in tqdm(data["train"], desc="Writing bookcorp.txt"):
+with open("./data/openwebtext.txt", "w", encoding="utf-8") as f:
+    for line in tqdm(data["train"], desc="Writing openwebtext.txt"):
         text = line["text"]
         if not text:
             continue
@@ -29,7 +29,6 @@ with open("./data/bookcorp.txt", "w", encoding="utf-8") as f:
         for k, v in replacements.items():
                 line = text.replace(k, v)
         if bytes_written + text_bytes > MAX_BYTES:
-            tqdm.write("WE BROKE AS FUCK")
             break
         f.write(text + " ")
         bytes_written += text_bytes
